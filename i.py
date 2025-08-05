452f533542149aba4ccfab6f148cc3c5a56ed7f0
Folk Thiraphong <thiraphongkhlangsap@gmail.com>
2025-08-05 23:38:14 +0700

    m

---
+++ i.py
@@ -0,0 +1,12 @@
+def format_strings(*args):
+    word = ''.join(args).upper() 
+    return word.replace(" ", "-").upper()
+pass
+
+if __name__ == '__main__':
+    result = format_strings("Hello", "world", "this", "is", "a", "test")
+    print(result)  # Output: "HELLOWORLDTHISISATEST"
+    result = format_strings("Python", "is", "fun")
+    print(result)  # Output: "PYTHONISFUN"
+    result = format_strings("Hello world")
+    print(result)  # Output: "HELLO-WORLD"
